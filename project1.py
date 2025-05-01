8abb80c6f3fcf3f817a9306d09ded8ed07a51ab8
Mominali55 <mominshaikh54849@gmail.com>
2025-05-01 17:22:13 +0530

    new concept list+

--- project1.py
+++ project1.py
@@ -37,6 +37,24 @@ f={
 for x,y in f.items():
     print(f"{x[:-1]}.{y}".upper().ljust(30),"30".upper().center(20,"-"))
 
+g="non-veg"
+print(g.center(49,"-").upper())
+# Now using list
+b=["butter chicken","chicken sharwma","butter nun"]
+b.insert(2,"kabab")
+b.insert(4,"kiddo")
+print("1."+b[0].upper().ljust(28),"200$".upper().center(20,"-"))
+print("2."+b[1].upper().ljust(28),"40$".upper().center(20,"-"))
+print("3."+b[2].upper().ljust(28),"1000$".upper().center(20,"-"))
+print("4."+b[3].upper().ljust(28),"1$".upper().center(20,"-"))
+print("5."+b[4].upper().ljust(28),"sold out".upper().center(20,"-"))
+
+thislist=["apple","banana","cherry"]
+for i in range(len(thislist)):
+    print(i,thislist[0][1])
+
+
+
 
 
 
