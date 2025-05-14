b55aaf6b7eec96120759c284cd806defd20cd10f
Mominali55 <mominshaikh54849@gmail.com>
2025-05-14 17:36:11 +0530

    project1+up1

--- .idea/project1.py
+++ .idea/project1.py
@@ -5,19 +5,26 @@ if b=='y' or b=='Y':
     print("gender is male".upper())
 else:
     print("gneder is female".upper())
+a = "*enter your address in the given format*"
+print(a.upper().center(70, "-"))
 def address(c):
-     a="*enter your address in the given format*"
-     print(a.upper().center(70,"-"))
-     b="state,city,pincode,flt,"
-     print(b.upper().center(70,"-"))
+    b = "state,city,pincode,flat"
+    print(b.upper().center(70, "-"))
+    print("Your entered address:", c)
 
-e=(address(input(str)))
-print("*is this your perfect address ?(y/n)*".upper(),address())
+# Input prompt from user
+user_input = input("Enter your address: ")
+address(user_input)
+
+print("*is this your perfect address ?(y/n)*".upper())
 d=str(input())
 if d=='y' or d=='Y':
     print("confirmed".upper())
 else:
     print("re-submission".upper())
+    user_input = input("Enter your address: ")
+    address(user_input)
+    print(user_input)
 
 
 
