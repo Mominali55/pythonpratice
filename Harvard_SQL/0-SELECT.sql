b2e9ab3985174b24d1cd0be54b8c6f798865f82f
Momin Shaikh <mominshaikh54849@gmail.com>
2025-12-20 10:02:48 +0530

    SELECT keyword is used to slect columns from tables..You can "*" for all select coloumn

---
+++ Harvard_SQL/0-SELECT.sql
@@ -0,0 +1,13 @@
+--The data is a set of collection(movies, books, etc)
+
+-- Select all columns form "longlist" table
+SELECT * FROM "longlist"; 
+
+-- Select "title" column from "longlist" table
+SELECT "title" FROM "longlist";
+
+-- Select "title" and "author" column from "longlist" table
+SELECT "title", "author" FROM "longlist";
+
+-- Select "title", "author", and "translator" column from "longlist" table
+SELECT "title", "author", "translator" FROM "longlist";
