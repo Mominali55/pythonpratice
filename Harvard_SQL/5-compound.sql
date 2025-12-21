678827a354c5f6c2b071274412d725a962141a4b
Momin Shaikh <mominshaikh54849@gmail.com>
2025-12-21 15:28:41 +0530

    Making are statement compound by using parenthesis,etc

---
+++ Harvard_SQL/5-compound.sql
@@ -0,0 +1,10 @@
+-- find tittles from 2022 or 2023
+SELECT "title","author","year" FROM "longlist" WHERE ("year" = 2022 OR "year" = 2023);
+
+-- find hardcover books from 2022 and 2023
+SELECT "title","author","year" FROM "longlist" WHERE ("year" = 2022 OR "year" = 2023) AND "format"="hardcover";
+
+-- Find books between 2019 and 2022
+-- Using many ORs
+SELECT "title", "year" FROM "longlist" 
+WHERE "year" = 2019 OR 2020 OR 2021 OR 2022; --or you can do by seprately Eg:- "year" = 2019 OR "year" = 2020 OR "year" = 2021 OR "year" = 2022
