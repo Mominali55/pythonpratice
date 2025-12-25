9a82fb3667d5a123a02cfdea7e8f3351040f3521
Momin Shaikh <mominshaikh54849@gmail.com>
2025-12-25 14:42:25 +0530

    File updated using one more api key in the same file and a little bit of use of "json" library for readibility..

--- Python_prog/library(sys,req)_api.py
+++ Python_prog/library(sys,req)_api.py
@@ -1,7 +1,9 @@
 import sys 
 import requests
+import json #importing json library to work with json data line: 32
 
 def main():
+    json_lib()
     print("Search the Art Institute of Chicago!")
     try:
         response = requests.get(
@@ -19,5 +21,22 @@ def main():
     for artwork in content["data"]:
         print(f"* {artwork['title']}") #printing the title of each artwork
 
+
+def json_lib():
+    if len(sys.argv) != 2:
+        sys.exit()
+    
+    response = requests.get(
+        "https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1] #Artist name passed as command line argument
+    )
+    # print(json.dumps(response.json(),indent=2)) #The json becomes more readible by explicitly telling intend by 2
+    temp=response.json()
+    for result in temp["results"]: #looping over each result in results list Eg:Taylor Swift songs
+        print(result["trackName"]) #printing the track name of each result
 main()
+
+
+# Note:
+# When you provide input from terminal the sys.argv takes its as a list so if the artist name has space Eg: Taylor swift it will consider it as two different arguments,
+# so to avoid that we can provide the name in quotes Eg:"Taylor Swift"
 
