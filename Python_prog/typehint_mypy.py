64b4ed6642b24d54c023a2159afe52eb3f3fd8a3
Momin Shaikh <mominshaikh54849@gmail.com>
2026-03-08 11:32:42 +0530

    Typehinting in detail

--- Python_prog/typehint_mypy.py
+++ Python_prog/typehint_mypy.py
@@ -1,6 +1,23 @@
-def meow(n: int):
+def meow(n: int) -> None:
     for _ in range(n):
         print("Meow!")
 
-number= input("Number: ")
-meow(number)  # This will cause a type error because input() returns a string, not an int.
+
+
+def main():
+    number : int = int(input("Number: "))
+    meows : str =meow_2(number)
+    print(meows, end="")  # end="" prevents adding an extra newline after printing the meows
+
+def meow_2(n : int) ->str:
+    return "Meow\n"* n
+
+
+
+if __name__ == "__main__":
+    main()
+
+    
+# Note:
+# 1. The type hints (like `n: int`) are just for readability and do not affect the actual execution of the code.
+# 2. The `meow` function does not return anything, so its return type is `None`. If you want to indicate that, you can use `-> None` in the function definition.
